c3702fa13c96151e2dbe0519bda2c7f5ecc732ef
RFK-BlueBell <rkwarta2111@comcast.net>
2020-01-20 20:41:02 -0500

    For info, added console.log to Server function

--- hello.js
+++ hello.js
@@ -5,23 +5,27 @@ const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
+	console.log(q);
+
 	var filename = "." + q.pathname;
 	if (filename == './') {filename = './index';}
 	
 	filename = filename + ".html";
 	console.log(filename);
+	
 	fs.readFile(filename, function(err, data) {
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/html'});
 			return res.end("404 Not Found");
-		} 
-		res.writeHead(200, {'Content-Type': 'text/html'});
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});  
 		res.write(data);
+		
 		return res.end();
 	});
 	
 }).listen(PORT);
 
 
-console.log("Server Listening on Port 8080...");
+console.log('Server Listening on Port ' + PORT);
 
